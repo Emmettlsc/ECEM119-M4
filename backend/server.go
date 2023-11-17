bf757705ac127db3b8c4d748f45656e2e0e5c7f9
Emmett Cocke <emmettlsc@gmail.com>
2023-11-16 17:50:06 -0800

    work pls

--- backend/server.go
+++ backend/server.go
@@ -2,39 +2,92 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
+	"sync"
 )
 
 var addr = flag.String("addr", "0.0.0.0:8080", "http service address")
 
-var upgrader = websocket.Upgrader{} // use default options
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // Allow all origins
+	},
+}
+
+// client represents a connected WebSocket client.
+type client struct {
+	conn *websocket.Conn
+	send chan []byte
+}
+
+var (
+	clients   = make(map[*client]bool) // Connected clients
+	broadcast = make(chan []byte)      // Broadcast channel
+	mu        sync.Mutex               // Mutex for clients
+)
 
 func echo(w http.ResponseWriter, r *http.Request) {
-	var upgrader = websocket.Upgrader{
-		CheckOrigin: func(r *http.Request) bool {
-			return true // Allow all origins
-		},
-	}
-	c, err := upgrader.Upgrade(w, r, nil)
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Print("upgrade:", err)
 		return
 	}
-	defer c.Close()
+	defer conn.Close()
+
+	cl := &client{conn: conn, send: make(chan []byte)}
+
+	mu.Lock()
+	clients[cl] = true
+	mu.Unlock()
+
+	go writePump(cl) // Start the writePump goroutine for this client
+
 	for {
-		mt, message, err := c.ReadMessage()
+		_, message, err := conn.ReadMessage()
 		if err != nil {
 			log.Println("read:", err)
 			break
 		}
 		log.Printf("recv: %s", message)
-		err = c.WriteMessage(mt, message)
-		if err != nil {
-			log.Println("write:", err)
-			break
+
+		// Broadcast the message to all clients
+		broadcast <- message
+	}
+
+	mu.Lock()
+	delete(clients, cl)
+	mu.Unlock()
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcast
+		fmt.Printf("Broadcasting message: %s\n", string(msg))
+		mu.Lock()
+		for cl := range clients {
+			select {
+			case cl.send <- msg:
+			default:
+				close(cl.send)
+				delete(clients, cl)
+			}
 		}
+		mu.Unlock()
+	}
+}
+
+func writePump(cl *client) {
+	defer cl.conn.Close()
+	for {
+		msg, ok := <-cl.send
+		if !ok {
+			cl.conn.WriteMessage(websocket.CloseMessage, []byte{})
+			return
+		}
+		cl.conn.WriteMessage(websocket.TextMessage, msg)
 	}
 }
 
@@ -42,5 +95,11 @@ func main() {
 	flag.Parse()
 	log.SetFlags(0)
 	http.HandleFunc("/echo", echo)
+	go handleMessages()
+
+	// Serve static files from the client directory
+	fs := http.FileServer(http.Dir("../client"))
+	http.Handle("/", fs)
+
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
--- m4/m4.ino
+++ m4/m4.ino
@@ -1,77 +1,217 @@
-#include <WiFiNINA.h>
-#include <ArduinoHttpClient.h>
+/****************************************************************************************************************************
+  WebSocketClient_NINA.ino
+  For boards using WiFiNINA Shield/Module
+
+  Blynk_WiFiNINA_WM is a library for the Mega, Teensy, SAM DUE, nRF52, STM32 and SAMD boards
+  (https://github.com/khoih-prog/Blynk_WiFiNINA_WM) to enable easy configuration/reconfiguration and
+  autoconnect/autoreconnect of WiFiNINA/Blynk
+
+  Based on and modified from WebSockets libarary https://github.com/Links2004/arduinoWebSockets
+  to support other boards such as  SAMD21, SAMD51, Adafruit's nRF52 boards, etc.
+
+  Built by Khoi Hoang https://github.com/khoih-prog/WebSockets_Generic
+  Licensed under MIT license
+
+  Created on: 24.05.2015
+  Author: Markus Sattler
+ *****************************************************************************************************************************/
+
+
+#define _WEBSOCKETS_LOGLEVEL_     2
+#define WEBSOCKETS_NETWORK_TYPE   NETWORK_WIFININA
+
+#include <WiFiNINA_Generic.h>
 #include <Arduino_LSM6DS3.h>
-#include <ArduinoJson.h>
+#include <WebSocketsClient_Generic.h>
 
+WebSocketsClient webSocket;
 
-char ssid[] = "UCLA_WEB";                // your network SSID (name)
-char pass[] = "";                        // your network password (use for WPA, or use as key for WEP)
-int status = WL_IDLE_STATUS;             // the Wi-Fi radio's status
+int status = WL_IDLE_STATUS;
 
-char serverAddress[] = "plantoml.com";     //webserver address 
-int port = 80;                            //webserver port
-WiFiClient wifi;
-HttpClient client = HttpClient(wifi, serverAddress, port);
+#define USE_SSL         false
 
-unsigned long previousMillis = 0;   
-const long interval = 1000;             
-const int batchSize = 10;
-int batchCounter = 0;               
-String batchData = "";
+#if USE_SSL
+  // Deprecated echo.websocket.org to be replaced
+  #define WS_SERVER           "44.207.82.113"
+  #define WS_PORT             8080
+#else
+  // To run a local WebSocket Server
+  #define WS_SERVER           "44.207.82.113"
+  #define WS_PORT             8080
+#endif
 
-void setup() {
-  Serial.begin(9600);
-  while (!Serial) { }
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 
-  pinMode(LED_BUILTIN, OUTPUT);
+char ssid[] = "UCLA_WEB";        // your network SSID (name)
+char pass[] = "";         // your network password (use for WPA, or use as key for WEP), length must be 8+
 
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to network: ");
-    Serial.println(ssid);
-    status = WiFi.begin(ssid, pass);
-    delay(5000);
-  }
+bool alreadyConnected = false;
 
-  if (!IMU.begin()) {
-    Serial.println("Failed to initialize IMU!");
-    while (1);
-  }
+void webSocketEvent(const WStype_t& type, uint8_t * payload, const size_t& length)
+{
+    switch (type)
+    {
+        case WStype_DISCONNECTED:
+            if (alreadyConnected)
+            {
+                Serial.println("[WSc] Disconnected!");
+                alreadyConnected = false;
+            }
+            break;
 
-  Serial.println("You're connected to the network and IMU initialized!");
-  Serial.println("---------------------------------------");
+        case WStype_CONNECTED:
+            {
+                alreadyConnected = true;
+                Serial.print("[WSc] Connected to url: ");
+                Serial.println((char *) payload);
+                // send a message to server when Connected
+                webSocket.sendTXT("Connected");
+            }
+            break;
+
+        case WStype_TEXT:
+            // Log the received text message
+            Serial.print("[WSc] Received text: ");
+            Serial.println((char *) payload);
+            // Optionally, handle received text messages here
+            break;
+
+        case WStype_BIN:
+            // Log binary messages (if needed)
+            Serial.print("[WSc] Received binary length: ");
+            Serial.println(length);
+            // Optionally, handle binary data here
+            break;
+
+        case WStype_PING:
+            // Log ping messages (if needed)
+            Serial.println("[WSc] Received ping");
+            break;
+
+        case WStype_PONG:
+            // Log pong messages (if needed)
+            Serial.println("[WSc] Received pong");
+            break;
+
+        default:
+            break;
+    }
 }
+
+
+void printWifiStatus()
+{
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("WebSockets Client IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+void setup()
+{
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+
+  while (!Serial);
+
+  Serial.print("\nStart WebSocketClient_NINA on ");
+  Serial.println(WEBSOCKETS_GENERIC_VERSION);
+
+  Serial.println("Used/default SPI pinout:");
+  Serial.print("MOSI:");
+  Serial.println(MOSI);
+  Serial.print("MISO:");
+  Serial.println(MISO);
+  Serial.print("SCK:");
+  Serial.println(SCK);
+  Serial.print("SS:");
+  Serial.println(SS);
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE)
+  {
+    Serial.println("Communication with WiFi module failed!");
+
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION)
+  {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED)
+  {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    //delay(10000);
+  }
+
+  printWifiStatus();
+
+  // server address, port and URL
+  Serial.print("WebSockets Server : ");
+  Serial.println(WS_SERVER);
+
+  // server address, port and URL
+  webSocket.begin(WS_SERVER, WS_PORT, "/echo");
+
+  // event handler
+  webSocket.onEvent(webSocketEvent);
+
+  // server address, port and URL
+  Serial.print("Connected to WebSockets Server @ ");
+  Serial.println(WS_SERVER);
+
+    if (!IMU.begin()) {
+      Serial.println("Failed to initialize IMU!");
+      while (1);
+  }
+  
+}
+
 
 void loop() {
-  float ax, ay, az;
-  unsigned long currentMillis = millis();
+    webSocket.loop();
 
-  if (IMU.accelerationAvailable()) {
-    IMU.readAcceleration(ax, ay, az);
+    static unsigned long lastSendTime = 0;
+    unsigned long currentMillis = millis();
 
-    if (batchCounter > 0) {
-      batchData += ",";
+    // Send data every 100 milliseconds
+    if (currentMillis - lastSendTime > 100) {
+        lastSendTime = currentMillis;
+
+        float ax, ay, az;
+        if (IMU.accelerationAvailable()) {
+            IMU.readAcceleration(ax, ay, az);
+
+            // Construct JSON-formatted string
+            String jsonMessage = "{\"type\": \"gyroscope\", \"value\": " + String(az) + "}";
+
+            // Send this message over WebSocket
+            webSocket.sendTXT(jsonMessage);
+        }
     }
-    batchData += String(ax);
-    batchCounter++;
-
-    if (batchCounter >= batchSize) {
-      //send the batch data to the server
-      client.beginRequest();
-      client.post("/data");
-      client.sendHeader("Content-Type", "text/plain");
-      client.sendHeader("Content-Length", batchData.length());
-      client.beginBody();
-      client.print(batchData);
-      client.endRequest();
-
-      batchData = "";
-      batchCounter = 0;
-
-      int statusCode = client.responseStatusCode();
-      String response = client.responseBody();
-      Serial.print("Status code: ");
-      Serial.println(statusCode);
-      Serial.println(response);
-    }
-  }
 }
+// void loop()
+// {
+//   webSocket.loop();
+// }
